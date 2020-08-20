32383b6f841a5ae99126a67ead20b9baf4d886a4
Amanda Evangelista <amandaevangelista@alu.ufc.br>
2020-08-19 23:59:44 -0300

    Update List.jsx

--- atividade_04/src/components/List.jsx
+++ atividade_04/src/components/List.jsx
@@ -3,9 +3,20 @@ import React, { Component } from 'react';
 export default class List extends Component {
     render() {
         return (
-            <div>
-                <p>List Component!!</p>
-            </div>
+            <div style={{ marginTop: 13 }}>
+                <h2>Listar Disciplina</h2>
+                    <table className="table table-striped" style={{ marginTop: 16 }}>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Curso</th>
+                                <th>Capacidade</th>
+                            </tr>
+                        </thead>
+                        
+                    </table>
+                    </div>
         )
     }
 }
